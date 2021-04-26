99164be89a7f5e31f1c558be6d8ec7ab50979b69
DB-25 <dhruvbardaiya@gmail.com>
2021-04-26 20:03:13 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Text(
+          'Educato\nComing Soon!!!',
+          style: TextStyle(
+              color: Colors.white, fontSize: 40, fontWeight: FontWeight.w800),
+        ),
+      ),
+    );
+  }
+}
